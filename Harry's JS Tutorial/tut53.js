42292ce37886ba71a22d4d872a1feba7b2b7a173
Abhie <abhikore5577@gmail.com>
2023-10-20 10:54:13 +0530

    All Files Added

---
+++ Harry's JS Tutorial/tut53.js
@@ -0,0 +1,31 @@
+console.log("This is tutorial 53");
+
+function greet(name, greettText){
+    console.log(greettText+" "+name)
+console.log(name+" is a good boy");
+}
+let name="Abhi";
+let name1="Harsh";
+let name2="Aary";
+let name3="Amit";
+let greettText="good Morning";
+greet(name, greettText);
+greet(name1, greettText);
+greet(name2, greettText);
+greet(name3, greettText);
+
+
+function sum(a,b,c){
+d=a+b+c;
+return d;
+}
+
+let returnval=sum(1,2,3);
+console.log(returnval)
+
+function multi(a,b,c){
+    d=a*b*c;
+    return d;
+}
+let re=multi(2,3,4);
+console.log(re)
